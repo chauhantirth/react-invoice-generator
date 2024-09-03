f70017a89017949f49255d456e9c24b968667e10
chauhantirth <chauhantirth40@gmail.com>
2024-09-03 20:27:05 +0530

    Add: PriceManager CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom"
 import Home from './components/Home'
-import PriceManager from './components/PriceManager'
+import PriceManager from './components/PriceManager/PriceManager'
 import {changeRoute} from './store/actions'
 import {  useDispatch, useSelector } from 'react-redux'
 require('dotenv').config()
