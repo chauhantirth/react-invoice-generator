c22c15333044465fe65dd52f64908fa342dfb33c
chauhantirth <chauhantirth40@gmail.com>
2024-09-06 03:42:34 +0530

    Add: Dashboard svg + link on Home Page.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,16 @@ function Home() {
 
   return (
     <div className="app">
-      <h1 className="center fs-30">React Invoice Generator</h1>
+      <div style={{display: 'flex', justifyContent:'space-between'}}>
+        <span style={{marginLeft:'200px'}}>
+          <h1 className="center fs-30">React Invoice Generator</h1>
+        </span>
+        <a href='/pricemanager' style={{display: 'flex', justifyContent: 'right', verticalAlign: 'right', textDecoration:'none', color:'black'}}>
+          <h4>Dashboard  </h4>
+          <img style={{width:'14px', height:'14px', marginTop:'16px'}}src='/external-link-svgrepo-com.svg' />
+        </a>
+      </div>
+      
       {topError ? (<h4 className="center" style={{ color: 'red' }}>{topError}</h4>) : (<></>)}
       <InvoicePage setTopError={setTopError}/>
     </div>
